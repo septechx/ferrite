2bbaccb48c10fbb249954e7a6095d1c208090e9a
septech <septechx@proton.me>
2025-04-29 18:55:49 +0200

    Add server download support for neoforge

--- src/structs.rs
+++ src/structs.rs
@@ -10,3 +10,18 @@ pub struct FabricLoaderEntryLoader {
     pub stable: bool,
     pub version: String,
 }
+
+#[derive(Debug, Deserialize)]
+pub struct NeoForgeLoaderMetadata {
+    pub versioning: NeoForgeLoaderVersioning,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct NeoForgeLoaderVersioning {
+    pub versions: NeoForgeVersions,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct NeoForgeVersions {
+    pub version: Vec<String>,
+}
